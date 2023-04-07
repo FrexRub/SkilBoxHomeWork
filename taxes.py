928536660c50502557ce33344a9df10782fda38b
Stanislav Rubtsov <frexrub@gmail.com>
2023-04-07 21:55:05 +0300

    fix: edit code_1

--- taxes.py
+++ taxes.py
@@ -1,71 +1,86 @@
 class Property:
-    name_taxes = 'Базового налога'
+    name_taxes = 'Базовый налог'
+
     def __init__(self, worth):
         self.set_worth(worth)
 
     def set_worth(self, worth):
-        if isinstance(worth, int) and worth > 0:
-            self.__worth = worth
-        else:
-            print('Стоимость должна быть числом больше нуля')
-
-    def __str__(self):
-        return 'размер {}: {}'.format(self.name_taxes, self.get_worth())
+        self.__worth = worth
 
     def get_worth(self):
         return self.__worth
+
+    def __str__(self):
+        return '{}: {}'.format(self.name_taxes, self.get_worth())
 
     def tax_calculation(self):
         pass
 
 
 class Apartment(Property):
-    name_taxes = 'налога на квартиру'
+    name_taxes = 'Налог на квартиру'
+
     def __init__(self, worth):
         super().__init__(worth),
 
-    def set_worth(self, worth):
-        if isinstance(worth, int) and worth > 0:
-            self.__worth = worth / 1000
-        else:
-            print('Стоимость должна быть числом больше нуля')
+    def tax_calculation(self):
+        return super().get_worth() / 1000
 
     def __str__(self):
-        return 'размер {}: {}'.format(self.name_taxes, self.get_worth())
-
-    # def tax_calculation(self):
-    #     return self.get_worth() / 1000
+        return '{}: {}'.format(self.name_taxes, round(self.tax_calculation(), 2))
 
 
 class Car(Property):
+    name_taxes = 'Налог на машину'
+
     def __init__(self, worth):
         super().__init__(worth)
 
     def tax_calculation(self):
-        return self.get_worth() / 200
+        return super().get_worth() / 200
+
+    def __str__(self):
+        return '{}: {}'.format(self.name_taxes, round(self.tax_calculation(), 2))
 
 
 class CountryHouse(Property):
+    name_taxes = 'Налог на дачу'
+
     def __init__(self, worth):
         super().__init__(worth)
 
     def tax_calculation(self):
-        return self.get_worth() / 500
+        return super().get_worth() / 500
+
+    def __str__(self):
+        return '{}: {}'.format(self.name_taxes, round(self.tax_calculation(), 2))
 
 
-while True:
-    int_text = input('Введите имеющуюся сумму денег: ')
-    if int_text.isdigit():
-        amount_money = int(int_text)
-        if amount_money > 0:
-            break
-    else:
-        print('Сумма должна быть положительным числом')
+def input_number(text):
+    while True:
+        int_text = input(text)
+        if int_text.isdigit():
+            result = float(int_text)
+            if result > 0:
+                break
+        else:
+            print('Сумма должна быть положительным числом')
+    return result
 
-int_worth = int(input('Введите стоимость имущества: '))
 
-worth_apartment = Apartment(int_worth)
-# worth_car = Car(int_worth)
-# worth_countryHouse = CountryHouse(int_worth)
+amount_money = input_number('Введите имеющуюся сумму денег: ')
+worth_apartment = input_number('Введите стоимость квартиры: ')
+worth_car = input_number('Введите стоимость машины: ')
+worth_countryhouse = input_number('Введите стоимость дачи: ')
 
-print(worth_apartment)
+tax_apartment = Apartment(worth_apartment)
+tax_car = Car(worth_car)
+tax_countryhouse = CountryHouse(worth_countryhouse)
+print()
+print(tax_apartment)
+print(tax_car)
+print(tax_countryhouse)
+
+summa_tax = tax_apartment.tax_calculation() + tax_car.tax_calculation() + tax_countryhouse.tax_calculation()
+if amount_money < summa_tax:
+    print('Для уплаты налога вам нехватает: {}'.format(summa_tax - amount_money))
